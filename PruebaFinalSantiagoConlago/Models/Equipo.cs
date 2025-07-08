67ae49d9b1783968264f664a93cb5d8bb86c8f9f
Santiago <santtyconla@gmail.com>
2025-07-08 08:35:27 -0400

    Creacion de models equipo

---
+++ PruebaFinalSantiagoConlago/Models/Equipo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace PruebaFinalSantiagoConlago.Models
+{
+    public class Equipo
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Dispositivo { get; set; }
+        public string Marca { get; set; }
+        public bool GarantiaActiva { get; set; }
+        public int VidaUtilMeses { get; set; }
+    }
+}
