a585286cc1b2acc7c63fd79e2f93c6b25addbc44
Santiago <santtyconla@gmail.com>
2025-07-08 09:39:40 -0400

    se configuró maui program

--- PruebaFinalSantiagoConlago/MauiProgram.cs
+++ PruebaFinalSantiagoConlago/MauiProgram.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
+using PruebaFinalSantiagoConlago.Services;
+using PruebaFinalSantiagoConlago.ViewModels;
+using PruebaFinalSantiagoConlago.Views;
 
 namespace PruebaFinalSantiagoConlago
 {
@@ -15,8 +18,23 @@ namespace PruebaFinalSantiagoConlago
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            // Registrar servicios
+            builder.Services.AddSingleton<DatabaseService>();
+            builder.Services.AddSingleton<FileService>(_ => new FileService("TuApellido")); // Cambiar por tu apellido
+
+            // Registrar ViewModels
+            builder.Services.AddTransient<DispositivoViewModel>();
+            builder.Services.AddTransient<ListaDispositivosViewModel>();
+            builder.Services.AddTransient<LogsViewModel>();
+
+            // Registrar Views
+            builder.Services.AddTransient<AgregarDispositivoPage>();
+            builder.Services.AddTransient<ListaDispositivosPage>();
+            builder.Services.AddTransient<LogsPage>();
+
+
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
